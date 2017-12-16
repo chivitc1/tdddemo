df94697bf719609f87bc55efdadbd1f3ded348f2
= <=>
2017-12-16 07:55:29 +0700

    refactor names into PersonName

--- src/main/java/com/example/tdddemo/refactor/OnceYouBuyYouStartCryingTelephone.java
+++ src/main/java/com/example/tdddemo/refactor/OnceYouBuyYouStartCryingTelephone.java
@@ -4,131 +4,84 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public class OnceYouBuyYouStartCryingTelephone {
+public class OnceYouBuyYouStartCryingTelephone
+{
 
-    private Map<String, String> names = new HashMap<String, String>();
-
-    //private Map<String, Integer> types = new HashMap<String, Integer>();
+    private static final String SPACE = " ";
+    private Map<String, ConnectionType> connectionTypeForNumberMap = new HashMap<String, ConnectionType>();
     private Map<String, Date> cd = new HashMap<String, Date>();
 
     private Map<ConnectionType, PhoneConnection> connectionForATypeMap
             = new HashMap<ConnectionType, PhoneConnection>();
 
-    public OnceYouBuyYouStartCryingTelephone(){
+    public OnceYouBuyYouStartCryingTelephone()
+    {
         initialize();
     }
 
-    protected void initialize() {
-        connectionForATypeMap.put(ConnectionType.TWO_G,
-                new TwoGConnection());
-        connectionForATypeMap.put(ConnectionType.THREE_G,
-                new ThreeGConnection());
+    protected void initialize()
+    {
+        connectionForATypeMap.put(ConnectionType.TWO_G, new TwoGConnection());
+        connectionForATypeMap.put(ConnectionType.THREE_G, new ThreeGConnection());
     }
 
     /**
-     This method activates a connection for a customer and stores different
-     information in following maps for future use names, types and cd. if
-     the connection type is 2G then requests TRY for a valid 2G number.
-     if portability is not an issue then TRY provides a valid number, that
-     number is stored for the customer. Then we activate the connection.
-     For 3G - user needs data plan , we don't ask TRY for 3G...we don't
-     have permission for 3G data in many cities, so we will hack TRY
-     database and assign an id. If TRY catches us then we will disconnect
-     the data plan and deactivate the customer.
 
-     Is there any legal consumer forum issue?
-     For 4G- we don't have 4th generation spectrum. we will provide 3G
-     with a wrapper of 4G
-     * @param firstName
-     * @param prefix
-     * @param middleName
-     * @param lastName
+     * @param name
      * @param z
-     * @param gen
+     * @param connectionType
      * @return
      **/
-    public String addConnection(String firstName, String prefix, String middleName,
-                                String lastName, Date z, ConnectionType gen) {
-        if (firstName == null || lastName == null || z == null)
+    public String addConnection(PersonName name, Date z, ConnectionType connectionType) {
+        if (name.getFirstName() == null || name.getLastName() == null || z == null)
             throw new RuntimeException();
-        String personName = "";
-        if (prefix != null) {
-            personName = personName + " " + prefix;
-            if (firstName != null)
-                personName = personName + " " + firstName;
-            if (middleName != null)
-                personName = personName + " " + middleName;
-            if (lastName != null)
-                personName = personName + lastName;
-        } else {
-            if (firstName != null)
-                personName = personName + " " + firstName;
-            if (middleName != null)
-                personName = personName + " " + middleName;
-            if (lastName != null)
-                personName = personName + lastName;
-        }
+        String personName = buildPersonName(name);
+
         String number = Number.next();
-        connectionForATypeMap.put(number, connectionType);
+        connectionTypeForNumberMap.put(number, connectionType);
         cd.put(number, z);
-        if (gen == ConnectionType.TWO_G) {
-            activate2GCon(number);
-            connectionForATypeMap.put(ConnectionType.TWO_G, );
-        } else if (gen == ConnectionType.THREE_G) {
-            activate3GCon(number);
-            types.put(number, THREE_G);
-        } else if (gen == ConnectionType.FOUR_G) {
-            activate4GCon(number);
-            types.put(number, FOUR_G);
-        } else {
+
+        PhoneConnection phoneConnection = connectionForATypeMap.get(connectionType);
+
+        if (connectionType == null) {
             throw new IllegalStateException();
         }
+        phoneConnection.activate(personName, number);
+
         return number;
     }
 
-    private void activate2GCon(String n) {
+    protected String buildPersonName(PersonName personName) {
+        StringBuffer sb = new StringBuffer();
+        if (personName.getPrefix() != null) {
+            sb.append(personName.getPrefix()).append(SPACE);
+        }
+        if (personName.getFirstName() != null) {
+            sb.append(personName.getFirstName()).append(SPACE);
+        }
+        if (personName.getMiddleName() != null) {
+            sb.append(personName.getMiddleName()).append(SPACE);
+        }
+        if (personName.getLastName() != null) {
+            sb.append(personName.getLastName());
+        }
 
-    }
-    private void activate3GCon(String n) {
-
-    }
-    private void activate4GCon(String n) {
-
+        return sb.toString();
     }
 
     /**
      * This method takes number as input and generates post paid bills
-     * @param n
+     * @param number
      * @return
      **/
-    public String bill(String n) {
-        Integer gen = types.get(n);
-        if (gen == null) {
+    public String bill(String number) {
+        ConnectionType connectionType = connectionTypeForNumberMap.get(number);
+        if (connectionType == null) {
             throw new RuntimeException();
         }
-        switch (gen.intValue()) {
-            case TWO_G:
-                return gen2GBill(n);
-            case THREE_G:
-                return gen3GBill(n);
-            case FOUR_G:
-                return gen4GBill(n);
-            default:
-                break;
-        }
-        return "";
-    }
 
-    private String gen4GBill(String n) {
-        return "";
-    }
-
-    private String gen3GBill(String n) {
-        return "";
-    }
-
-    private String gen2GBill(String n) {
-        return "";
+        PhoneConnection phoneConnection = connectionForATypeMap.get(connectionType);
+        return phoneConnection.generateBillFor(number);
     }
 
     public void chargeIncomingSms(String num) {
--- src/test/java/com/example/tdddemo/refactor/OnceYouBuyYouStartCryingTelephoneTest.java
+++ src/test/java/com/example/tdddemo/refactor/OnceYouBuyYouStartCryingTelephoneTest.java
@@ -1,5 +1,6 @@
 package com.example.tdddemo.refactor;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
@@ -8,60 +9,80 @@ import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertNotNull;
 
-public class OnceYouBuyYouStartCryingTelephoneTest {
+public class OnceYouBuyYouStartCryingTelephoneTest
+{
     OnceYouBuyYouStartCryingTelephone telephone = new
             OnceYouBuyYouStartCryingTelephone();
+    PersonName name = null;
 
+    @Before
+    public void setUp()
+    {
+        name = new PersonName();
+    }
     @Test(expected = RuntimeException.class)
-    public void when_input_first_name_isnull_then_throws_exception() {
-        telephone.addConnection("1", null, null, null, null, 0);
-        fail("Code should not reach here");
-    }
-
-    @Test(expected=RuntimeException.class)
-    public void when_input_prefix_is_null_then_throws_exception()
-            throws Exception {
-        telephone.addConnection("a", null, null, null, null, 0);
-        fail("code should not reach here");
-    }
-
-    @Test(expected=RuntimeException.class)
-    public void when_input_last_name_is_null_then_throws_exception()
-            throws Exception {
-        telephone.addConnection("a", null, null, "c", null, 0);
+    public void when_input_first_name_is_null_then_throws_exception() throws Exception
+    {
+        telephone.addConnection(name, null, null);
         fail("code should not reach here");
     }
 
     @Test(expected = RuntimeException.class)
-    public void when_input_gen_is_invalid_then_throws_exception()
+    public void when_input_lasst_name_is_null_then_throws_exception() throws Exception
+    {
+        name.setFirstName("John");
+        telephone.addConnection(name, null, null);
+        fail("code should not reach here");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void when_input_z_is_null_then_throws_exception() throws Exception
+    {
+        name.setFirstName("John");
+        name.setLastName("Doe");
+        telephone.addConnection(name, null, null);
+        fail("code should not reach here");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void when_input_connection_type_is_null_then_throws_exception()
             throws Exception {
-        telephone.addConnection("a", null, null, "c", new Date(), 0);
+        name.setFirstName("John");
+        name.setLastName("Doe");
+        telephone.addConnection(name, new Date(), null);
         fail("code should not reach here");
     }
 
     @Test
     public void when_valid_input_then_adds_inputs() throws
             Exception {
-        assertNotNull(telephone.addConnection("a", null, null, "c", new
-                Date(), OnceYouBuyYouStartCryingTelephone.THREE_G));
+        name.setFirstName("John");
+        name.setLastName("Doe");
+        assertNotNull(telephone.addConnection(name, new Date(), ConnectionType.THREE_G));
     }
 
-//    @Test
-//    public void when_all_name_attributes_are_passed_then_forms_the_name()
-//            throws Exception {
-//        String johnsFirstName = "john";
-//        String johnsLastName = "smith";
-//        String johnsMiddleName = "maddison";
-//        String johnsNamePrefix = "dr.";
-//        String number = telephone.add(johnsFirstName,
-//                johnsNamePrefix,johnsMiddleName,
-//                johnsLastName, new Date(),
-//                OnceYouBuyYouStartCryingTelephone.FOUR_G);
-//        assertNotNull(number);
-//        String billDetails = telephone.bill(number);
-//        assertTrue(billDetails.contains(johnsNamePrefix));
-//        assertTrue(billDetails.contains(johnsLastName));
-//        assertTrue(billDetails.contains(johnsMiddleName));
-//        assertTrue(billDetails.contains(johnsFirstName));
-//    }
+    @Test
+    public void when_all_name_attributes_are_passed_then_forms_the_name() throws Exception {
+        String firstName = "john";
+        String lastName = "smith";
+        String middleName = "addison";
+        String prefix = "dr.";
+
+        name.setFirstName(firstName);
+        name.setLastName(lastName);
+        name.setMiddleName(middleName);
+        name.setPrefix(prefix);
+
+        String number = telephone.addConnection(name, new Date(), ConnectionType.TWO_G);
+
+        assertNotNull(number);
+
+        String billDetails = telephone.bill(number);
+        assertTrue(billDetails.contains(firstName));
+        assertTrue(billDetails.contains(lastName));
+        assertTrue(billDetails.contains(middleName));
+        assertTrue(billDetails.contains(prefix));
+
+    }
+
 }
