0f93f8bd2691b2a4b186aa90e7bb4e6230dbf537
chivitc1 <chivitc1@gmail.com>
2017-12-16 17:23:19 +0700

    Catalogue can store price for each procedure

--- src/main/java/com/example/tdddemo/healthcare/ServiceCatalogue.java
+++ src/main/java/com/example/tdddemo/healthcare/ServiceCatalogue.java
@@ -2,13 +2,17 @@ package com.example.tdddemo.healthcare;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ServiceCatalogue {
     private List<Procedure> procs = new ArrayList<>();
+    private Map<String, BigDecimal> prices = new HashMap<>();
 
-    public void add(Procedure proc, BigDecimal ten) {
+    public void add(Procedure proc, BigDecimal price) {
         this.procs.add(proc);
+        this.prices.put(proc.getId(), price);
     }
 
     public Procedure find(String id) {
@@ -16,6 +20,13 @@ public class ServiceCatalogue {
             if (proc.getId().equals(id)) {
                 return proc;
             }
+        }
+        return null;
+    }
+
+    public BigDecimal findPriceBy(String id) {
+        if (prices.containsKey(id)) {
+            return this.prices.get(id);
         }
         return null;
     }
--- src/test/java/com/example/tdddemo/healthcare/ServiceCatalogueTest.java
+++ src/test/java/com/example/tdddemo/healthcare/ServiceCatalogueTest.java
@@ -1,5 +1,6 @@
 package com.example.tdddemo.healthcare;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -9,6 +10,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class ServiceCatalogueTest {
+
     @Test
     public void user_can_add_a_service_to_catalogue() {
         Procedure proc = new Procedure("1234", "Basic Oxygen Setup");
@@ -36,5 +38,17 @@ public class ServiceCatalogueTest {
         catalogue.add(proc2, BigDecimal.TEN);
         assertNotNull("Expected a procedure 1234", catalogue.find("1234"));
         assertNotNull("Expected a procedure 6789", catalogue.find("6789"));
+    }
+
+    @Test
+    public void catalogue_returns_procedure_and_price_for_a_configured_procedure() {
+        Procedure proc1 = new Procedure("1234", "Basic Oxygen Setup");
+        Procedure proc2 = new Procedure("6789", "Basic Oxygen Setup");
+        ServiceCatalogue catalogue = new ServiceCatalogue();
+        catalogue.add(proc1, BigDecimal.TEN);
+        catalogue.add(proc2, BigDecimal.ONE);
+
+        assertEquals(BigDecimal.TEN, catalogue.findPriceBy(proc1.getId()));
+        assertEquals(BigDecimal.ONE, catalogue.findPriceBy(proc2.getId()));
     }
 }
