099482df3484acadecaa36799eccc6862053166f
chivitc1 <chivitc1@gmail.com>
2017-12-16 11:55:21 +0700

    Add support multi procedures in Catalogue

--- src/main/java/com/example/tdddemo/healthcare/ServiceCatalogue.java
+++ src/main/java/com/example/tdddemo/healthcare/ServiceCatalogue.java
@@ -1,17 +1,21 @@
 package com.example.tdddemo.healthcare;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServiceCatalogue {
-    private Procedure proc;
+    private List<Procedure> procs = new ArrayList<>();
 
     public void add(Procedure proc, BigDecimal ten) {
-        this.proc = proc;
+        this.procs.add(proc);
     }
 
     public Procedure find(String id) {
-        if (this.proc.getId().equals(id)) {
-            return this.proc;
+        for (Procedure proc : procs) {
+            if (proc.getId().equals(id)) {
+                return proc;
+            }
         }
         return null;
     }
--- src/test/java/com/example/tdddemo/healthcare/ServiceCatalogueTest.java
+++ src/test/java/com/example/tdddemo/healthcare/ServiceCatalogueTest.java
@@ -27,5 +27,14 @@ public class ServiceCatalogueTest {
         assertNull(catalogue.find("4567"));
     }
 
-
+    @Test
+    public void catalogue_returns_procedure_for_a_configured_procedure_id() {
+        Procedure proc1 = new Procedure("1234", "Basic Oxygen Setup");
+        Procedure proc2 = new Procedure("6789", "Basic Oxygen Setup");
+        ServiceCatalogue catalogue = new ServiceCatalogue();
+        catalogue.add(proc1, BigDecimal.TEN);
+        catalogue.add(proc2, BigDecimal.TEN);
+        assertNotNull("Expected a procedure 1234", catalogue.find("1234"));
+        assertNotNull("Expected a procedure 6789", catalogue.find("6789"));
+    }
 }
